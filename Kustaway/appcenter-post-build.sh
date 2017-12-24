2670eda706c991698d0fd6448a051bfda2eb7a14
amay077 <okuokuoku@gmail.com>
2017-12-24 21:13:06 +0900

    Update

--- Kustaway/appcenter-post-build.sh
+++ Kustaway/appcenter-post-build.sh
@@ -4,6 +4,6 @@ echo "Upload to DeployGate"
 
 curl \
     -F "token=ad2141e095d70b00f12ed1ceeb25e6e55a42d26a" \
-    -F "file=@$MOBILECENTER_OUTPUT_DIRECTORY/app-release.apk" \
+    -F "file=@$MOBILECENTER_OUTPUT_DIRECTORY/Kustaway-release.apk" \
     -F "message=build by Visual Studio Mobile Center - #$MOBILECENTER_BUILD_ID" \
     https://deploygate.com/api/users/amay077/apps
