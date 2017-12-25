c663aeed5cec60d537b613e0d62fc3d0adf9d12b
amay077 <okuokuoku@gmail.com>
2017-12-26 00:46:53 +0900

    Fix Material 適用したら画像プレビューが落ちてた

--- Kustaway/src/main/java/net/amay077/kustaway/ProfileActivity.java
+++ Kustaway/src/main/java/net/amay077/kustaway/ProfileActivity.java
@@ -7,7 +7,6 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
